be4e6c6943e9b97d25cbf840a49756d9c49e9479
Kristina2605D <109177697+Kristina2605D@users.noreply.github.com>
2022-10-01 23:43:20 +0400

    first commit

---
+++ routs/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+
+import { createUser, getUsers, deleteUser, updateUser } from "../controllers/users.js";
+
+const router = express.Router();
+
+
+//all routes in here are starting with /users
+router.get("/", getUsers);
+
+
+
+router.post("/", createUser);
+// /users/2 => req.params {id: 2}
+
+router.delete("/:id", deleteUser);
+router.patch("/:id", updateUser);
+
+export default router;
